882fbe433a55f1bc10a80a81bb2919121a57818b
Lin Chen-Hsin <5905597+linchenhsin@users.noreply.github.com>
2025-03-10 23:26:03 +0800

    Feat: Handle player assignment when player number != 12 closes #3 (#5)
    feat: handle player assignment when player number != 12 closes #3

--- src/utils/dev.ts
+++ src/utils/dev.ts
@@ -1,7 +1,7 @@
 import { createPlayer } from "./player";
 
 // convenient helper functions for dev
-export const createDummyPlayers = async (num: number) => {
+export const createDummyPlayers = async (num: number, offset = 0) => {
   const names = [
     "Alice",
     "Bob",
@@ -15,8 +15,10 @@ export const createDummyPlayers = async (num: number) => {
     "Jack",
     "Karl",
     "Lily",
+    "Mike",
+    "Nancy",
   ];
-  for (let i = 0; i < num; i++) {
+  for (let i = offset; i < offset + num; i++) {
     await createPlayer(names[i]);
   }
 };
