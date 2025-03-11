b7a1650889271a2abd198b44b6b3f0a7a883cb83
Po-Cheng Yeh <blp100x@hotmail.com>
2025-03-11 22:18:21 +0800

    refactor: rename import path

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { StrictMode } from "react";
 
-import Admin from "./pages/Admin.tsx";
+import Admin from "./pages/Admin";
 import Home from "./pages/Home";
-import Join from "./pages/Join.tsx";
-import NotFound from "./pages/NotFound.tsx";
-import Player from "./pages/Player.tsx";
+import Join from "./pages/Join";
+import NotFound from "./pages/NotFound";
+import Player from "./pages/Player";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- src/utils/game.ts
+++ src/utils/game.ts
@@ -6,7 +6,7 @@ import {
   PLAYER_TYPE,
 } from "../constants";
 
-import { useData, setData, updateData } from "./firebaseHelpers.ts";
+import { useData, setData, updateData } from "./firebaseHelpers";
 
 import { IGame } from "../interfaces";
 
