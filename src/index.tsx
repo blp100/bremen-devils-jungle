59b33e34886cb3c8c9e0d9ea506d6bec978955fb
Po-Cheng Yeh <blp100x@hotmail.com>
2025-08-20 17:43:20 +0800

    feat: add qrcode ui for reconnetcion links

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
-import { createRoot } from "react-dom/client"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { StrictMode } from "react"
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { StrictMode } from "react";
 
-import Admin from "./pages/Admin"
-import Home from "./pages/Home"
-import Join from "./pages/Join"
-import NotFound from "./pages/NotFound"
-import Player from "./pages/Player"
-import { Toaster } from "@/components/ui/sonner"
-import { ThemeProvider } from "@/components/ThemeProvider"
+import Admin from "./pages/Admin";
+import Home from "./pages/Home";
+import Join from "./pages/Join";
+import NotFound from "./pages/NotFound";
+import Player from "./pages/Player";
+import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import AdminJoinLinks from "./components/AdminJoinLinks";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -17,6 +18,7 @@ createRoot(document.getElementById("root")!).render(
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="admin" element={<Admin />} />
+          <Route path="admin/join-links" element={<AdminJoinLinks />} />
           <Route path="join" element={<Join />} />
           <Route path="player/:playerId" element={<Player />} />
           <Route path="*" element={<NotFound />} />
@@ -25,4 +27,4 @@ createRoot(document.getElementById("root")!).render(
       </BrowserRouter>
     </ThemeProvider>
   </StrictMode>,
-)
+);
